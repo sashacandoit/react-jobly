884d1e116e93773957fdf7b47ffd295705f05557
Sasha Czerniawski <sashacandoit@gmail.com>
2023-02-02 08:27:32 -0500

    base url updated again

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://jobly-backend-2.onrender.com";
+// const BASE_URL = process.env.REACT_APP_BASE_URL || "https://jobly-backend-2.onrender.com";
+
+const BASE_URL = "https://jobly-backend-2.onrender.com";
 
 /** API Class.
  *
